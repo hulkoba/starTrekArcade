0b66c4388e7c453e0a846de343129d9ba5b1585b
Ben Stuerz <b.stuerz@gmx.de>
2015-10-25 11:18:27 +0100

    Changed the scene, scale, one enemy, tried lookat and createEnemy

---
+++ Star Trek Arcade/Assets/Skripte/CameraScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public float SmoothTime = 1f;
+
+	private Vector3 CameraVelocity;
+
+	private Transform Target = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// only if target exists
+		if (Target != null) {
+
+			// rotate towards the given target, damped a little bit
+			transform.LookAt(transform.position + Vector3.SmoothDamp(transform.forward, Target.position - transform.position, ref CameraVelocity, SmoothTime));
+		}
+	}
+
+	// called by the creation script
+	public void SetTarget(Transform NewTarget){
+		Target = NewTarget;
+	}
+}
---
+++ Star Trek Arcade/Assets/Skripte/CreationScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreationScript : MonoBehaviour {
+
+	public GameObject Example;
+	public float WaitTime = 2f;
+
+	private CameraScript Cam;
+	private Transform Group;
+
+	// Use this for initialization
+	void Start () {
+		Cam = GameObject.Find ("Main Camera").GetComponent<CameraScript>();
+		Group = GameObject.Find ("Objects").transform;
+		StartCoroutine (CreateGameObject ());
+	}
+
+	private IEnumerator CreateGameObject(){
+
+		// wait for some time
+		yield return new WaitForSeconds (WaitTime);
+
+		// if originals exists
+		if (Example != null) {
+
+			// create new GameObject
+			GameObject NewGO = Instantiate (Example);
+			// set new random position
+			NewGO.transform.position = new Vector3(Random.Range(-10,10),Random.Range(0,3), Random.Range(-2,10));
+			NewGO.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value);
+			// name it
+			NewGO.name = "Cube_" + Group.childCount;
+			// set group as parent
+			NewGO.transform.parent = Group;
+			// tell the camera the new target
+			Cam.SetTarget(NewGO.transform);
+		}
+
+		// call this function again
+		StartCoroutine (CreateGameObject ());
+	}
+}
