c3ef9b8f995b14d8d27ccc28375d9898891b6af3
Ben Stuerz <b.stuerz@gmx.de>
2015-11-01 19:33:23 +0100

    EnemyHealthAnfang

---
+++ Star Trek Arcade/Assets/Skripte/EnemyHealth.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : HealthController {
+
+    private EnemyRadar enemyRadar;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
