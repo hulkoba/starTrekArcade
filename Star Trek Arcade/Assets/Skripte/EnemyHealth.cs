233c51f55511ed627914af66709d2c4470fdf781
Benjamin Bela Stuerz <b.stuerz@gmx.de>
2015-11-03 15:16:23 +0100

    EnemieHealthFehlerWeg

--- Star Trek Arcade/Assets/Skripte/EnemyHealth.cs
+++ Star Trek Arcade/Assets/Skripte/EnemyHealth.cs
@@ -6,13 +6,6 @@ public class EnemyHealth : HealthController {
     private EnemyRadar enemyRadar;
 	private GameObject GameController;
 
-	IEnumerator dying(){
-		// wait for some time
-		//yield return new WaitForSeconds (4f);
-		//GameController.GetComponent<CreationScript>().enemyCounter -= 1;
-		//Destroy (this.gameObject);
-	}
-
 	// Use this for initialization
 	void Start () {
 		GameController = GameObject.Find("Game_Controller");
