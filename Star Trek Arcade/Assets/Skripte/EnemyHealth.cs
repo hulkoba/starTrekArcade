c301b1e5d9146d2b39ca0285cbc96854c0de4290
Benjamin Bela Stuerz <b.stuerz@gmx.de>
2015-11-03 14:57:17 +0100

    Utility

--- Star Trek Arcade/Assets/Skripte/EnemyHealth.cs
+++ Star Trek Arcade/Assets/Skripte/EnemyHealth.cs
@@ -8,15 +8,15 @@ public class EnemyHealth : HealthController {
 
 	IEnumerator dying(){
 		// wait for some time
-		yield return new WaitForSeconds (4f);
-		GameController.GetComponent<CreationScript>().enemyCounter -= 1;
-		Destroy (this.gameObject);
+		//yield return new WaitForSeconds (4f);
+		//GameController.GetComponent<CreationScript>().enemyCounter -= 1;
+		//Destroy (this.gameObject);
 	}
 
 	// Use this for initialization
 	void Start () {
 		GameController = GameObject.Find("Game_Controller");
-		dying ();
+		//dying ();
 	}
 	
 	// Update is called once per frame
