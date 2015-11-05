b003ecf513c0440440a9bed0c07ede0a74e0376d
Ben Stuerz <b.stuerz@gmx.de>
2015-11-05 07:16:00 +0100

    simpleFollowScriptForTheAI

---
+++ Star Trek Arcade/Assets/Skripte/Enemy/EnemyAI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+	
+	public Transform player;
+	public float playerDistance;
+	public float rotationDamping;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.Find("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerDistance = Vector3.Distance(player.position, transform.position);
+		
+		if(playerDistance < 15f){
+			lookAtPlayer();
+		}
+		if(playerDistance < 12f){
+			chase();
+		}
+	}
+	
+	void lookAtPlayer(){
+		//transform.LookAt(Player);
+		Quanternion rotation = Quanternion.LookRotation (player.position - transform.position);
+		transform.rotation = Quanternion.Slerp(transform.rotation,rotation,Time.deltaTime * rotationDamping);
+	}
+	
+	void chase(){
+		transform.Translate(Vector3.forward = moveSpeed * Time.deltaTime);
+	}
+}
