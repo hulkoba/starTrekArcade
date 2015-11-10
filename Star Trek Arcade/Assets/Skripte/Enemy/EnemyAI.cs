60965934e7cf2119af78029e7cd485f14d04181b
Ben Stuerz <b.stuerz@gmx.de>
2015-11-10 12:42:31 +0100

    Ohne Fehler

--- Star Trek Arcade/Assets/Skripte/Enemy/EnemyAI.cs
+++ Star Trek Arcade/Assets/Skripte/Enemy/EnemyAI.cs
@@ -39,19 +39,19 @@ public class EnemyAI : MonoBehaviour {
 	//Spieler als Sphere oder Rigit Body einbauen, der Enemy hat selbst einen Sphere Collider
 	//Sobald der Spieler in den Collider kommt, wird das hier ausgelöst, TriggerEnter
 	//WICHTIG: TriggerEnter 
-	void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Astroid"))
-        {
-            obstacleDetected = true;
-        }
-    }
+	// void OnTriggerEnter(Collider other)
+    // {
+        // if (other.gameObject.CompareTag("Astroid"))
+        // {
+            // obstacleDetected = true;
+        // }
+    // }
 
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("Astroid"))
-        {
-            obstacleDetected = false;
-        }
-    }
+    // void OnTriggerExit(Collider other)
+    // {
+        // if (other.gameObject.CompareTag("Astroid"))
+        // {
+            // obstacleDetected = false;
+        // }
+    // }
 }
