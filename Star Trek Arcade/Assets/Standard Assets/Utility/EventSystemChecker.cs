cebef286b0f2e53475a159b45194177d12a171a1
Jacoba <jacoba.brandner@web.de>
2015-11-10 12:42:05 +0100

    bugfix in standard asstes EventSystemChecker?

--- Star Trek Arcade/Assets/Standard Assets/Utility/EventSystemChecker.cs
+++ Star Trek Arcade/Assets/Standard Assets/Utility/EventSystemChecker.cs
@@ -5,18 +5,29 @@ using UnityEngine.EventSystems;
 
 public class EventSystemChecker : MonoBehaviour
 {
-    //public GameObject eventSystem;
+    public GameObject eventSystem;
 
-	// Use this for initialization
-	void Awake ()
-	{
-	    if(!FindObjectOfType<EventSystem>())
-        {
-           //Instantiate(eventSystem);
-            GameObject obj = new GameObject("EventSystem");
-            obj.AddComponent<EventSystem>();
-            obj.AddComponent<StandaloneInputModule>().forceModuleActive = true;
-            obj.AddComponent<TouchInputModule>();
-        }
-	}
+
+     // Use this for initialization
+     IEnumerator Start () {
+         yield return new WaitForEndOfFrame();
+
+         if (!FindObjectOfType<EventSystem>())
+         {
+            Instantiate(eventSystem);
+         }
+     }
+
+	// // Use this for initialization
+	// void Awake ()
+	// {
+	//     if(!FindObjectOfType<EventSystem>())
+ //        {
+ //           //Instantiate(eventSystem);
+ //            GameObject obj = new GameObject("EventSystem");
+ //            obj.AddComponent<EventSystem>();
+ //            obj.AddComponent<StandaloneInputModule>().forceModuleActive = true;
+ //            obj.AddComponent<TouchInputModule>();
+ //        }
+ //	}
 }
