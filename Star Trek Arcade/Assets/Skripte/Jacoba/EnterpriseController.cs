5f57c91c22868a84378c816da2618f1ecfaa7515
Jacoba <jacoba.brandner@web.de>
2015-12-01 13:20:00 +0100

    fly on shift & warp on w

--- Star Trek Arcade/Assets/Skripte/Jacoba/EnterpriseController.cs
+++ Star Trek Arcade/Assets/Skripte/Jacoba/EnterpriseController.cs
@@ -10,9 +10,6 @@ using Random = UnityEngine.Random;
     public class EnterpriseController : MonoBehaviour
     {
 
-        [SerializeField] private float _flySpeed;
-        [SerializeField] private float _warpSpeed;  // left shift for running
-
         [SerializeField] private bool m_UseFovKick;
         [SerializeField] private FOVKick m_FovKick = new FOVKick();
 
@@ -20,7 +17,9 @@ using Random = UnityEngine.Random;
         [SerializeField] private AudioClip _explosionSound;
 
         private Camera _camera;
-        private bool _isFlying;
+        private bool _isMoving;
+        private float _flySpeed = 10;   // left shift for flying
+        private float _warpSpeed = 40;  // w for warping
         //private ArrowLook _ArrowLook;
 
         private float m_YRotation;
@@ -86,7 +85,7 @@ using Random = UnityEngine.Random;
         private void Start() {
             m_CharacterController = GetComponent<CharacterController>();
             _camera = Camera.main;
-            _isFlying = true;
+            _isMoving = true;
             m_OriginalCameraPosition = _camera.transform.localPosition;
             m_FovKick.Setup(_camera);
 
@@ -95,102 +94,95 @@ using Random = UnityEngine.Random;
         }
 
 
-
         // Update is called once per frame
-        private void Update()
-        {
+        private void Update() {
             RotateView();
             //m_MoveDir.y = 0f;
         }
 
-        private void FixedUpdate()
-        {
+        private void FixedUpdate() {
             float speed;
             GetInput(out speed);
             // always move along the camera forward as it is the direction that it being aimed at
-            Vector3 desiredMove = transform.forward*m_Input.y + transform.right*m_Input.x;
+            Vector3 desiredMove = transform.forward * speed;
+            //+ transform.right * m_Input.x
 
             // get a normal for the surface that is being touched to move along it
             RaycastHit hitInfo;
             Physics.SphereCast(transform.position, m_CharacterController.radius, Vector3.down, out hitInfo,
                                m_CharacterController.height/2f);
+
             desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
 
-            m_MoveDir.x = desiredMove.x*speed;
-            m_MoveDir.z = desiredMove.z*speed;
+            //m_MoveDir.x = desiredMove.x * speed;
+            m_MoveDir.z = desiredMove.z * speed;
 
-            if(!_isFlying) {
-                PlayWarpSound();
-            }
-
-            //
-            // if (m_CharacterController.isGrounded) {
-            //     m_MoveDir.y = -m_StickToGroundForce;
-            //
-            // } else {
-
-            m_MoveDir += Physics.gravity * 0 * Time.fixedDeltaTime;
-    //    }
-            m_CollisionFlags = m_CharacterController.Move(m_MoveDir*Time.fixedDeltaTime);
+            m_CollisionFlags = m_CharacterController.Move(m_MoveDir * Time.fixedDeltaTime);
 
             UpdateCameraPosition();
         }
 
+        private void RotateView() {
+             /*_ArrowLook.*/LookRotation (transform, _camera.transform);
+        }
 
-        private void UpdateCameraPosition()
-        {
+        private void UpdateCameraPosition() {
             Vector3 newCameraPosition = _camera.transform.localPosition;
             newCameraPosition.y = m_OriginalCameraPosition.y;
             _camera.transform.localPosition = newCameraPosition;
         }
 
 
-        private void GetInput(out float speed)
-        {
-            // Read input
-            float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
-            float vertical = CrossPlatformInputManager.GetAxis("Vertical");
+        private void GetInput(out float speed) {
 
-            bool wasflying = _isFlying;
+            bool wasflying = _isMoving;
+            speed = 0f;
 
 #if !MOBILE_INPUT
-            // On standalone builds, walk/run speed is modified by a key press.
+
             // keep track of whether or not the enterprise is flying or 'warping''
-            _isFlying = !Input.GetKey(KeyCode.LeftShift);
-#endif
+            _isMoving = !Input.GetKey(KeyCode.LeftShift) || !Input.GetKey(KeyCode.W);
+
             // set the desired speed to be flying or 'warping'
-            speed = _isFlying ? _flySpeed : _warpSpeed;
-            m_Input = new Vector2(horizontal, vertical);
+            if(Input.GetKeyDown(KeyCode.W)) {
+                PlayWarpSound();
+            }
+            if(Input.GetKey(KeyCode.W)) {
+                speed = _warpSpeed;
+            }
+
+            if(Input.GetKey(KeyCode.LeftShift)) {
+                speed = _flySpeed;
+            }
+#endif
+            //speed = _isMoving ? _flySpeed : _warpSpeed;
+
+
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+
+            m_Input = new Vector2(h, v);
 
             // normalize input if it exceeds 1 in combined length:
-            if (m_Input.sqrMagnitude > 1) {
-                m_Input.Normalize();
-            }
+             if (m_Input.sqrMagnitude > 1) {
+                 m_Input.Normalize();
+             }
 
             // handle speed change to give an fov kick
             // only if the player is going to a run, is running and the fovkick is to be used
-            if (_isFlying != wasflying && m_UseFovKick && m_CharacterController.velocity.sqrMagnitude > 0) {
+            if (_isMoving != wasflying && m_UseFovKick && m_CharacterController.velocity.sqrMagnitude > 0) {
                 StopAllCoroutines();
-                StartCoroutine(!_isFlying ? m_FovKick.FOVKickUp() : m_FovKick.FOVKickDown());
+                StartCoroutine(!_isMoving ? m_FovKick.FOVKickUp() : m_FovKick.FOVKickDown());
             }
         }
 
-
-        private void RotateView()
-        {
-             /*_ArrowLook.*/LookRotation (transform, _camera.transform);
-        }
-
-
-
-        private void PlayWarpSound()
-        {
+        private void PlayWarpSound() {
             m_AudioSource.clip = _warpSound;
             m_AudioSource.Play();
         }
 
-        private void PlayExplosionSound()
-        {
+        private void PlayExplosionSound() {
             m_AudioSource.clip = _explosionSound;
             m_AudioSource.Play();
         }
@@ -198,7 +190,7 @@ using Random = UnityEngine.Random;
 
         private void OnControllerColliderHit(ControllerColliderHit hit)
         {
-            Console.WriteLine("### Collision!  " + hit);
+            print("### Collision!  " + hit);
             Rigidbody body = hit.collider.attachedRigidbody;
             //dont move the rigidbody if the character is on top of it
             if (m_CollisionFlags == CollisionFlags.Below) {
