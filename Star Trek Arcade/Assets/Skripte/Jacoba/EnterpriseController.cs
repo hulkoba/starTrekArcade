2b1175cdcb8fdf4b7742fbe8ead95217ba7ded0e
Jacoba <jacoba.brandner@web.de>
2015-12-05 18:00:23 +0100

    cleaned up a bit

--- Star Trek Arcade/Assets/Skripte/Jacoba/EnterpriseController.cs
+++ Star Trek Arcade/Assets/Skripte/Jacoba/EnterpriseController.cs
@@ -25,7 +25,7 @@ using Random = UnityEngine.Random;
 
         private CharacterController m_CharacterController;
         private CollisionFlags m_CollisionFlags;
-        private Vector3 m_OriginalCameraPosition;
+        private Vector3 _originalCamPosition;
         private AudioSource m_AudioSource;
 
         // shots:
@@ -34,55 +34,10 @@ using Random = UnityEngine.Random;
         private float nextFire = 0.5f;
         public float fireRate;
 
-//------------------------------------------------------------------------------------------------
-        //ArrowLook
-        // TODO: extra file
-        public bool clampVerticalRotation = true;
-        private float MinimumX = -360F;
-        private float MaximumX = 360F;
+        //cam rotation
         private float smoothTime = 5f;
-
         private Quaternion m_CharacterTargetRot;
         private Quaternion m_CameraTargetRot;
-
-
-        public void InitArrowLook(Transform character, Transform camera) {
-            m_CharacterTargetRot = character.localRotation;
-            m_CameraTargetRot = camera.localRotation;
-        }
-
-        public void LookRotation(Transform character, Transform camera) {
-
-            float yRot = Input.GetAxis("Horizontal");
-            float xRot = Input.GetAxis("Vertical");
-
-            m_CharacterTargetRot *= Quaternion.Euler (0f, yRot, 0f);
-            m_CameraTargetRot *= Quaternion.Euler (-xRot, 0f, 0f);
-
-            if(clampVerticalRotation)
-                m_CameraTargetRot = ClampRotationAroundXAxis (m_CameraTargetRot);
-
-            character.localRotation = Quaternion.Slerp (character.localRotation, m_CharacterTargetRot,
-                    smoothTime * Time.deltaTime);
-                camera.localRotation = Quaternion.Slerp (camera.localRotation, m_CameraTargetRot,
-                    smoothTime * Time.deltaTime);
-        }
-
-        Quaternion ClampRotationAroundXAxis(Quaternion q){
-            q.x /= q.w;
-            q.y /= q.w;
-            q.z /= q.w;
-            q.w = 1.0f;
-
-            float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan (q.x);
-
-            angleX = Mathf.Clamp (angleX, MinimumX, MaximumX);
-
-            q.x = Mathf.Tan (0.5f * Mathf.Deg2Rad * angleX);
-
-            return q;
-        }
-// -------------------------------------------------------------------------------------------------------------------------
 
 
         // Use this for initialization
@@ -90,21 +45,22 @@ using Random = UnityEngine.Random;
             m_CharacterController = GetComponent<CharacterController>();
             _camera = Camera.main;
             _isMoving = true;
-            m_OriginalCameraPosition = _camera.transform.localPosition;
+            _originalCamPosition = _camera.transform.localPosition;
             m_FovKick.Setup(_camera);
-
             m_AudioSource = GetComponent<AudioSource>();
-        /*_ArrowLook.*/InitArrowLook(transform , _camera.transform);
+
+            m_CharacterTargetRot = transform.localRotation;
+            m_CameraTargetRot = _camera.transform.localRotation;
         }
 
 
         // Update is called once per frame
         private void Update() {
-            RotateView();
+            RotateView(transform, _camera.transform);
             //pressed the firebutton AND loaded weapons?
             if(Input.GetButton("Fire1") && Time.time >= nextFire ) {
                 nextFire = Time.time + fireRate;
-                Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+                Instantiate(shot, _camera.transform.position, _camera.transform.rotation);
                 PlayShotSound();
             }
         }
@@ -122,25 +78,36 @@ using Random = UnityEngine.Random;
 
             forward = Vector3.ProjectOnPlane(forward, hitInfo.normal).normalized;
 
-            if(speed == _warpSpeed) {
+            //if(speed == _warpSpeed) {
             //    _moveDir.z = desiredMove.z * speed;
-
-            } else {
+            //} else {
             //    _moveDir.z = desiredMove.z * speed;
-            }
+            //}
             forward *= speed;
             m_CollisionFlags = m_CharacterController.Move(forward * Time.deltaTime);
 
             UpdateCameraPosition();
         }
 
-        private void RotateView() {
-             /*_ArrowLook.*/LookRotation (transform, _camera.transform);
+        public void RotateView(Transform character, Transform camera) {
+
+            float yRot = Input.GetAxis("Horizontal");
+            float xRot = Input.GetAxis("Vertical");
+
+            m_CharacterTargetRot *= Quaternion.Euler (0f, yRot, 0f);
+            m_CameraTargetRot *= Quaternion.Euler (-xRot, 0f, 0f);
+
+            character.localRotation = Quaternion.Slerp (character.localRotation, m_CharacterTargetRot,
+                smoothTime * Time.deltaTime);
+            camera.localRotation = Quaternion.Slerp (camera.localRotation, m_CameraTargetRot,
+                smoothTime * Time.deltaTime);
         }
+
 
         private void UpdateCameraPosition() {
             Vector3 newCameraPosition = _camera.transform.localPosition;
-            newCameraPosition.y = m_OriginalCameraPosition.y;
+            newCameraPosition.y = _originalCamPosition.y;
+            newCameraPosition.x = _originalCamPosition.x;
             _camera.transform.localPosition = newCameraPosition;
         }
 
@@ -183,6 +150,7 @@ using Random = UnityEngine.Random;
 
         private void PlayShotSound() {
             m_AudioSource.clip = _shotSound;
+            m_AudioSource.volume = 0.2f;
             m_AudioSource.Play();
         }
 
