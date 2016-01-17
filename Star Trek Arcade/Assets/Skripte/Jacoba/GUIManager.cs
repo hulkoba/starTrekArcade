bb14ca5d9311c8faa6b57283f9086ce2bb3ed1b0
Jacoba <jacoba.brandner@web.de>
2016-01-17 14:44:04 +0100

    round minimap

---
+++ Star Trek Arcade/Assets/Skripte/Jacoba/GUIManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIManager : MonoBehaviour {
+
+	public RenderTexture MiniMapTexture;
+	public Material MiniMapMaterial;
+	float offset;
+
+	// Use this for initialization
+	void Awake () {
+		offset = 10f;
+	}
+
+	void OnGUI () {
+		if(Event.current.type == EventType.Repaint) {
+			Graphics.DrawTexture(new Rect(Screen.width - 256 - offset, offset, 256, 256), MiniMapTexture, MiniMapMaterial);
+		}
+	}
+}
