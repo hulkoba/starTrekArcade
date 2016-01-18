e117e42f31d10f5df2cc2c1f24d246be1a197cbd
Ben Stuerz <b.stuerz@gmx.de>
2016-01-18 15:06:37 +0100

    Kollision mit Abstoßen

--- Star Trek Arcade/Assets/Skripte/Player/PlayerCollider.cs
+++ Star Trek Arcade/Assets/Skripte/Player/PlayerCollider.cs
@@ -16,6 +16,7 @@ public class PlayerCollider : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other){
+
 		if(other.tag == "EnterpriseBolt") {
 			return;
 		}
@@ -25,6 +26,14 @@ public class PlayerCollider : MonoBehaviour {
 			//Zerstoere Schuss
 			//Debug.Log ("PLAYERHEALTH:"+other.gameObject.name);
 			Destroy(other.gameObject);
+		}
+		if (other.gameObject.name == "Enemy(Clone)") {
+			Vector3 helper = gameObject.transform.forward;
+			other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(helper.x*20f,helper.y*20f,helper.z*20f),ForceMode.Impulse);
+		}
+		if (other.gameObject.name == "Asteroid(Clone)") {
+			Vector3 helper = gameObject.transform.forward;
+			other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(helper.x*20f,helper.y*20f,helper.z*20f),ForceMode.Impulse);
 		}
 	}
 }
